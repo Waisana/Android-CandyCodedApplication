879dbd915d28daf897378d2aec50c1b5dfe63874
Waisana Bosco <37265882+Waisana@users.noreply.github.com>
2019-11-09 08:47:45 -0800

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -2,6 +2,7 @@ package com.pluralsight.candycoded;
 
 import android.net.Uri;
 import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
